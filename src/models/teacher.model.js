a66335748db9c1b912f55f23b80eb8217cd3aeab
German Pinto <93947560+germanp007@users.noreply.github.com>
2023-10-13 19:10:57 -0300

    Update teacher.model.js

--- src/models/teacher.model.js
+++ src/models/teacher.model.js
@@ -1,1 +1,13 @@
+import mongoose from "mongoose";
 
+const teacherCollection = 'teachers';
+const teacherSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  lastname: {
+    type: String,
+    required: true
+  }
+})
